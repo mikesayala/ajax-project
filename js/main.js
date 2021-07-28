a57f0b0faf5f2c143397f117819cb8319c56449e
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-28 17:52:10 +0000

    make dummy content for the search results to mold how the DOM tree should look.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// var $form = document.querySelector('.form');
+// function genreSearch(value) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://api.jikan.moe/v3/genre/anime/' + value);
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function () {
+//   });
+//   xhr.send();
+// }
+
+// function shuffle(array) {
+//   for (var i = 0; i < array.length; i++) {
+//     var randomPosition = Math.floor(Math.random() * array.length);
+//     var placeHolder = array[i];
+//     array[i] = array[randomPosition];
+//     array[randomPosition] = placeHolder;
+//   }
+//   return array;
+// }
+
+// function handleSearch(event) {
+//   event.preventDefault();
+//   var genreForm = {
+//     value: $form.elements.select.value
+//   };
+//   genreSearch($form.elements.select.value);
+// }
+
+// function genreDOMCreation (genreForm) {
+
+// }
+
+// $form.addEventListener('submit', handleSearch);
